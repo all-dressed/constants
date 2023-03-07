b890828aa16f5c7b41608d70d611b411e745c8c3
Chin Leung <chin@swoo.ca>
2023-03-07 15:20:40 -0500

    Added missing translations

---
+++ lang/en/delivery-schedule-frequencies.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '7' => 'Every week',
+    '14' => 'Every 2 weeks',
+    '28' => 'Every 4 weeks',
+    '56' => 'Every 8 weeks',
+];
---
+++ lang/en/subscription-frequencies.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '1' => 'Daily',
+    '7' => 'Weekly',
+    '14' => 'Bi-weekly',
+    'other' => 'Every :number days',
+];
