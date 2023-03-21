2ff2a45ab5105469b5bbdbb53f58c6de94a47a2e
Catherine <ccharette.dev@gmail.com>
2023-03-21 12:42:10 -0400

    Add update package permission

--- src/Permission.php
+++ src/Permission.php
@@ -36,6 +36,7 @@ enum Permission: string
     case UPDATE_DISCOUNT = 'discounts:update';
     case UPDATE_MENU = 'menus:update';
     case UPDATE_ORDER = 'orders:update';
+    case UPDATE_PACKAGE = 'packages:update';
     case UPDATE_PAYMENT_GATEWAY = 'gateways:update';
     case UPDATE_PAYMENT_METHOD = 'customers:payment-methods:update';
     case UPDATE_PRODUCT = 'products:update';
