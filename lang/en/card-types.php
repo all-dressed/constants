c60457132232307f23540fe4a98c65c8dd359fa5
Chin Leung <chin@swoo.ca>
2023-03-25 02:08:08 -0400

    Added translation to card types

---
+++ lang/en/card-types.php
@@ -0,0 +1,11 @@
+<?php
+
+use AllDressed\Constants\CardType;
+
+return [
+    CardType::AMEX->value => 'American Express',
+    CardType::DISCOVER->value => 'Discover',
+    CardType::MASTERCARD->value => 'Mastercard',
+    CardType::OTHER->value => 'Unknown',
+    CardType::VISA->value => 'Visa',
+];
--- src/CardType.php
+++ src/CardType.php
@@ -4,10 +4,11 @@ namespace AllDressed\Constants;
 
 use AllDressed\Constants\Concerns\AvailableAsCollection;
 use AllDressed\Constants\Concerns\CanBeRandomized;
+use AllDressed\Constants\Concerns\HasTranslation;
 
 enum CardType: string
 {
-    use AvailableAsCollection, CanBeRandomized;
+    use AvailableAsCollection, CanBeRandomized, HasTranslation;
 
     case AMEX = 'amex';
     case DISCOVER = 'discover';
