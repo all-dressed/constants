f9b3924fc62ce5daf988aff24ff120aa8d0006f8
Chin Leung <chin@swoo.ca>
2023-03-28 14:19:10 -0400

    Added translation for address types

---
+++ lang/en/address-types.php
@@ -0,0 +1,9 @@
+<?php
+
+use AllDressed\Constants\AddressType;
+
+return [
+    AddressType::APARTMENT->value => 'Apartment',
+    AddressType::BUSINESS->value => 'Business',
+    AddressType::HOUSE->value => 'House',
+];
