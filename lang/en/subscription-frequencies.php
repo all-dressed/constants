547cbe0b919abb48069e5e1225ec4a93019ca3f7
Chin Leung <chin@swoo.ca>
2023-03-29 11:06:35 -0400

    Updated subscription frequencies translations

--- lang/en/subscription-frequencies.php
+++ lang/en/subscription-frequencies.php
@@ -1,8 +1,12 @@
 <?php
 
+use AllDressed\Constants\DeliveryScheduleFrequency;
+
 return [
-    '1' => 'Daily',
-    '7' => 'Weekly',
-    '14' => 'Bi-weekly',
+    1 => 'Daily',
+    DeliveryScheduleFrequency::WEEKLY->value => 'Every week',
+    DeliveryScheduleFrequency::BIWEEKLY->value => 'Every 2 weeks',
+    DeliveryScheduleFrequency::EVERY_FOUR_WEEKS->value => 'Every 4 weeks',
+    DeliveryScheduleFrequency::EVERY_EIGHT_WEEKS->value => 'Every 8 weeks',
     'other' => 'Every :number days',
 ];
