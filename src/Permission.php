23e9fe4d339e36a24a4fb5e41c15885a69735960
Chin Leung <chin@swoo.ca>
2023-04-06 10:53:33 -0400

    Added permission for cancelling an order

--- src/Permission.php
+++ src/Permission.php
@@ -13,6 +13,7 @@ enum Permission: string
     case ACTIVATE_DELIVERY_SCHEDULE = 'delivery-schedules:activate';
     case ACTIVATE_DELIVERY_ZONE = 'delivery-zones:activate';
     case BILL_SUBSCRIPTION = 'subscriptions:bill';
+    case CANCEL_ORDER = 'orders:cancel';
     case CANCEL_SUBSCRIPTION = 'subscriptions:cancel';
     case CREATE_API_TOKEN = 'tokens:api:create';
     case CREATE_CATEGORY = 'categories:create';
