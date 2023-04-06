a1f897b1e1f61548795b821ae9bf59e99b6673d6
Chin Leung <chin@swoo.ca>
2023-04-05 20:36:13 -0400

    Added permission to create credit

--- src/Permission.php
+++ src/Permission.php
@@ -16,6 +16,7 @@ enum Permission: string
     case CANCEL_SUBSCRIPTION = 'subscriptions:cancel';
     case CREATE_API_TOKEN = 'tokens:api:create';
     case CREATE_CATEGORY = 'categories:create';
+    case CREATE_CREDIT = 'credits:create';
     case CREATE_CUSTOMER = 'customers:create';
     case CREATE_DELIVERY_ZONE = 'delivery-zones:create';
     case CREATE_DISCOUNT = 'discounts:create';
