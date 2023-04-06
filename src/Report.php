f6f1108e4bc4cfeb096ea8bc2a4fda8186165581
Chin Leung <chin@swoo.ca>
2023-04-06 11:33:14 -0400

    Added reports

---
+++ src/Report.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AllDressed\Constants;
+
+use AllDressed\Constants\Concerns\AvailableAsCollection;
+use AllDressed\Constants\Concerns\CanBeRandomized;
+
+enum Report: string
+{
+    use AvailableAsCollection, CanBeRandomized;
+
+    case BC_ITEMS_SOLD = 'bc-items-sold';
+}
