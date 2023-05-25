69bf84ca835413fbbea9f9d75917ef4fea7096b5
Chin Leung <chin@swoo.ca>
2023-05-25 15:39:55 -0400

    Added orders activities

--- src/Activity.php
+++ src/Activity.php
@@ -7,6 +7,8 @@ enum Activity: string
     case ADDED_SUBSCRIPTION = 'subscriptions:new';
     case CANCELLED_SUBSCRIPTION = 'subscriptions:cancelled';
     case PAUSED_SUBSCRIPTION = 'subscriptions:paused';
+    case PLACED_ORDER = 'orders:new';
     case REACTIVATED_SUBSCRIPTION = 'subscriptions:reactived';
+    case REFUNDED_ORDER = 'orders:refunded';
     case RESUMED_SUBSCRIPTION = 'subscriptions:resumed';
 }
