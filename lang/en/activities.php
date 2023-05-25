7c33190b80edb5bd1914f34df7ad89f3efc9857d
Chin Leung <chin@swoo.ca>
2023-05-25 15:24:16 -0400

    Fixed translation

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -6,5 +6,5 @@ return [
     Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
     Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
     Activity::PAUSED_SUBSCRIPTION->value => ':name has paused a subscription.',
-    Activity::RESUMED_SUBSCRIPTION->value => ':name has paused a subscription.',
+    Activity::RESUMED_SUBSCRIPTION->value => ':name has resumed a subscription.',
 ];
