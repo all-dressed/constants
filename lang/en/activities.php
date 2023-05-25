6bd36fa0799a2ce4d98dbf5a7e4707711a5ddc92
Chin Leung <chin@swoo.ca>
2023-05-25 15:32:25 -0400

    Added constant for subscription reactivations

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -6,5 +6,6 @@ return [
     Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
     Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
     Activity::PAUSED_SUBSCRIPTION->value => ':name has paused a subscription.',
+    Activity::REACTIVATED_SUBSCRIPTION->value => ':name has reactivated a subscription.',
     Activity::RESUMED_SUBSCRIPTION->value => ':name has resumed a subscription.',
 ];
--- src/Activity.php
+++ src/Activity.php
@@ -7,5 +7,6 @@ enum Activity: string
     case ADDED_SUBSCRIPTION = 'subscriptions:new';
     case CANCELLED_SUBSCRIPTION = 'subscriptions:cancelled';
     case PAUSED_SUBSCRIPTION = 'subscriptions:paused';
+    case REACTIVATED_SUBSCRIPTION = 'subscriptions:reactived';
     case RESUMED_SUBSCRIPTION = 'subscriptions:resumed';
 }
