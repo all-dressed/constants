9a5eb59ef08f54e2bc806b6866543ed9151ebf2e
Chin Leung <chin@swoo.ca>
2023-05-25 12:08:49 -0400

    Added label of activities

---
+++ lang/en/activities.php
@@ -0,0 +1,10 @@
+<?php
+
+use AllDressed\Constants\Activity;
+
+return [
+    Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
+    Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
+    Activity::PAUSED_SUBSCRIPTION->value => ':name has paused a subscription.',
+    Activity::RESUMED_SUBSCRIPTION->value => ':name has paused a subscription.',
+];
