12086451766035524f393581e7d34703729e5038
Chin Leung <chin@swoo.ca>
2023-05-25 15:45:33 -0400

    Use amount instead of value

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -6,8 +6,8 @@ return [
     Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
     Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
     Activity::PAUSED_SUBSCRIPTION->value => ':name has paused a subscription.',
-    Activity::PLACED_ORDER->value => ':name has placed a new order of :value :currency.',
+    Activity::PLACED_ORDER->value => ':name has placed a new order of :amount :currency.',
     Activity::REACTIVATED_SUBSCRIPTION->value => ':name has reactivated a subscription.',
-    Activity::REFUNDED_ORDER->value => ':name has received a refund of :value :currency.',
+    Activity::REFUNDED_ORDER->value => ':name has received a refund of :amount :currency.',
     Activity::RESUMED_SUBSCRIPTION->value => ':name has resumed a subscription.',
 ];
