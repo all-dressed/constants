82909e3cb50745f90d00eec67868e9e974ab2b68
Yu Qiao Chen <yuqiao@swoo.ca>
2023-07-27 14:19:11 -0400

    Changed ordering

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -4,10 +4,10 @@ use AllDressed\Constants\Activity;
 
 return [
     Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
+    Activity::CANCELLED_ORDER->value => ':name has cancelled an order.',
     Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
     Activity::PAUSED_SUBSCRIPTION->value => ':name has paused a subscription.',
     Activity::PLACED_ORDER->value => ':name has placed a new order of :amount :currency.',
-    Activity::CANCELLED_ORDER->value => ':name has cancelled an order.',
     Activity::REACTIVATED_SUBSCRIPTION->value => ':name has reactivated a subscription.',
     Activity::REFUNDED_ORDER->value => ':name has received a refund of :amount :currency.',
     Activity::RESUMED_SUBSCRIPTION->value => ':name has resumed a subscription.',
--- src/Activity.php
+++ src/Activity.php
@@ -5,10 +5,10 @@ namespace AllDressed\Constants;
 enum Activity: string
 {
     case ADDED_SUBSCRIPTION = 'subscriptions:new';
+    case CANCELLED_ORDER = 'orders:cancelled';
     case CANCELLED_SUBSCRIPTION = 'subscriptions:cancelled';
     case PAUSED_SUBSCRIPTION = 'subscriptions:paused';
     case PLACED_ORDER = 'orders:new';
-    case CANCELLED_ORDER = 'orders:cancelled';
     case REACTIVATED_SUBSCRIPTION = 'subscriptions:reactived';
     case REFUNDED_ORDER = 'orders:refunded';
     case RESUMED_SUBSCRIPTION = 'subscriptions:resumed';
