71d4c58b035a37be29589050053f95eacfc67142
Gabriel Oabel <gabriel@swoo.ca>
2023-10-04 11:33:47 -0400

    added activity constant for failed payments

--- src/Activity.php
+++ src/Activity.php
@@ -7,6 +7,7 @@ enum Activity: string
     case ADDED_SUBSCRIPTION = 'subscriptions:new';
     case CANCELLED_ORDER = 'orders:cancelled';
     case CANCELLED_SUBSCRIPTION = 'subscriptions:cancelled';
+    case FAILED_SUBSCRIPTION_PAYMENT = 'subscriptions:failed-payment';
     case PAUSED_SUBSCRIPTION = 'subscriptions:paused';
     case PLACED_ORDER = 'orders:new';
     case REACTIVATED_SUBSCRIPTION = 'subscriptions:reactived';
