927535cfcabf09f210a6cd3fbd8d6b8cfa20f565
Chin Leung <hello@chinleung.com>
2023-10-23 14:28:53 -0400

    Added permissions for gift cards

--- src/Permission.php
+++ src/Permission.php
@@ -74,9 +74,11 @@ enum Permission: string
     case VIEW_DELIVERY_ZONES = 'delivery-zones:index';
     case VIEW_DISCOUNT = 'discounts:show';
     case VIEW_DISCOUNTS = 'discounts:index';
+    case VIEW_GIFT_CARD = 'gift-cards:show';
+    case VIEW_GIFT_CARDS = 'gift-cards:index';
     case VIEW_INTEGRATIONS = 'integrations:index';
-    case VIEW_KLAVIYO_INTEGRATIONS = 'integrations:klaviyo:index';
     case VIEW_INVOICES = 'invoices:index';
+    case VIEW_KLAVIYO_INTEGRATIONS = 'integrations:klaviyo:index';
     case VIEW_ITEMS = 'items:index';
     case VIEW_LTV = 'ltv:show';
     case VIEW_MENU = 'menus:show';
