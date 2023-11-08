9f048410c2601904686eb6e18f46ec78fab488ec
Gabriel Oabel <gabriel@swoo.ca>
2023-11-08 13:34:15 -0500

    added string values for gift cards

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -3,6 +3,7 @@
 use AllDressed\Constants\Activity;
 
 return [
+    Activity::ACTIVATED_GIFT_CARD->value => ':name has activated the :code gift card.',
     Activity::ADDED_SUBSCRIPTION->value => ':name has added a new subscription.',
     Activity::CANCELLED_ORDER->value => ':name has cancelled an order.',
     Activity::CANCELLED_SUBSCRIPTION->value => ':name has cancelled a subscription.',
@@ -12,5 +13,6 @@ return [
     Activity::REACTIVATED_SUBSCRIPTION->value => ':name has reactivated a subscription.',
     Activity::REFUNDED_ORDER->value => ':name has received a refund of :amount :currency.',
     Activity::RESUMED_SUBSCRIPTION->value => ':name has resumed a subscription.',
+    Activity::SENT_GIFT_CARD->value => ':name has send the :code gift card.',
     Activity::SKIPPED_MENU->value => ':name has skipped the delivery of :date for :subscription.',
 ];
