1a4a863e50108fff5841bb964fc8fa89d0e58448
Gabriel Oabel <gabriel@swoo.ca>
2023-12-05 11:42:52 -0500

    Added translation trait to ThirdPartyService

--- src/ThirdPartyService.php
+++ src/ThirdPartyService.php
@@ -2,8 +2,12 @@
 
 namespace AllDressed\Constants;
 
+use AllDressed\Constants\Concerns\HasTranslation;
+
 enum ThirdPartyService: string
 {
+    use HasTranslation;
+
     case BECOOL = 'becool';
     case KLAVIYO = 'klaviyo';
     case NETSUITE = 'netsuite';
