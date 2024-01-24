077a8e75575e9c492a3648ca1917731391451256
Willy Southiphonh <willy@swoo.ca>
2024-01-24 15:18:25 -0500

    :sparkle: Add CanBeRandomized for Integration::Factory

--- src/ThirdPartyService.php
+++ src/ThirdPartyService.php
@@ -2,11 +2,12 @@
 
 namespace AllDressed\Constants;
 
+use AllDressed\Constants\Concerns\CanBeRandomized;
 use AllDressed\Constants\Concerns\HasTranslation;
 
 enum ThirdPartyService: string
 {
-    use HasTranslation;
+    use HasTranslation, CanBeRandomized;
 
     case BECOOL = 'becool';
     case KLAVIYO = 'klaviyo';
