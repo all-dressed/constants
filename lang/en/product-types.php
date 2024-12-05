e2a5c8ce76cf3ff43abe6102d88ed96022091e64
Gabriel Balabanian <gbalabanian@swoo.ca>
2024-12-05 08:51:09 -0500

    added pet-food and pet-toy to lang/en/product-types

--- lang/en/product-types.php
+++ lang/en/product-types.php
@@ -5,4 +5,6 @@ use AllDressed\Constants\ProductType;
 return [
     ProductType::EDIBLE->value => 'Edible',
     ProductType::GENERIC->value => 'Generic',
+    ProductType::PET_FOOD->value => 'Pet-food',
+    ProductType::PET_TOY->value => 'Pet-toy',
 ];
