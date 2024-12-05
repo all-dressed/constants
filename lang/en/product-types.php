c7139f71f1c607e81ee557c37d1886983eda55b3
Gabriel Balabanian <gbalabanian@swoo.ca>
2024-12-05 09:33:32 -0500

    made the requested changes to product-types

--- lang/en/product-types.php
+++ lang/en/product-types.php
@@ -5,6 +5,6 @@ use AllDressed\Constants\ProductType;
 return [
     ProductType::EDIBLE->value => 'Edible',
     ProductType::GENERIC->value => 'Generic',
-    ProductType::PET_FOOD->value => 'Pet-food',
-    ProductType::PET_TOY->value => 'Pet-toy',
+    ProductType::PET_FOOD->value => 'Pet Food',
+    ProductType::PET_TOY->value => 'Pet Toy',
 ];
