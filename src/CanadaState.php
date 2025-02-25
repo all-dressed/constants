9dd89f80b88ed47a279e578a803b6f0be09511a5
Kevin Purahoo <kevin@swoo.ca>
2025-02-25 13:13:59 -0500

    Update the states constant for the states to be available per country

---
+++ src/CanadaState.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AllDressed\Constants;
+
+use AllDressed\Constants\Concerns\AvailableAsCollection;
+use AllDressed\Constants\Concerns\AvailableAsOptions;
+use AllDressed\Constants\Concerns\CanBeRandomized;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Lang;
+
+enum CanadaState: string
+{
+    use AvailableAsCollection, AvailableAsOptions, CanBeRandomized;
+
+    case ALBERTA = 'AL';
+    case BRITISH_COLUMBIA = 'BC';
+    case MANITOBA = 'MB';
+    case NEW_BRUNSWICK = 'NB';
+    case NEWFOUNDLAND_AND_LABRADOR = 'NL';
+    case NOVA_SCOTIA = 'NS';
+    case NORTHWEST_TERRITORIES = 'NT';
+    case NUNAVUT = 'NU';
+    case ONTARIO = 'ON';
+    case PRINCE_EDWARD_ISLAND = 'PE';
+    case QUEBEC = 'QC';
+    case SASKATCHEWAN = 'SK';
+    case YUKON = 'YK';
+
+
+    /**
+     * Retrieve the country of the state.
+     */
+    public function getCountry(): string
+    {
+        return 'CA';
+    }
+
+    /**
+     * Retrieve the abbreviation of the state.
+     */
+    public function getStateValue(): string
+    {
+        return $this->value;
+    }
+}
