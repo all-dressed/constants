7ea115afdd12604db4383507d396633c31d7aa97
Luukhainn <78034917+Luukhainn@users.noreply.github.com>
2025-10-04 12:22:54 -0400

    Add TripleWhale integration

--- lang/en/third-party-services.php
+++ lang/en/third-party-services.php
@@ -8,5 +8,6 @@ return [
     ThirdPartyService::KLAVIYO->value => 'Klaviyo',
     ThirdPartyService::LEGACY_CUSTOMER_NOTES->value => 'Legacy Customer Notes',
     ThirdPartyService::NETSUITE->value => 'NetSuite',
+    ThirdPartyService::TRIPLE_WHALE->value => 'Triple Whale',
     ThirdPartyService::ZENDESK->value => 'Zendesk',
 ];
